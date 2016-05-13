5f3c999e84e31eff13b41dfd9fc05d0b908bcb29
KadoTomohiro <kado.tomohiro@jp.fujitsu.com>
2016-05-13 10:51:05 +0900

    httpクライアント機能の追加

--- src/app/noisy-guy.component.ts
+++ src/app/noisy-guy.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import {Http, RequestOptionsArgs, HTTP_PROVIDERS} from '@angular/http';
 
 @Component({
   moduleId: module.id,
   selector: 'noisy-guy-app',
+  viewProviders: [HTTP_PROVIDERS],
   templateUrl: 'noisy-guy.component.html',
   styleUrls: ['noisy-guy.component.css']
 })
 export class NoisyGuyAppComponent {
   title = 'noisy-guy works!';
+
+  http: Http;
+  result;
+  talkedText: string;
+
+  constructor(http: Http) {
+    this.http = http;
+  }
+
+  talk() {
+
+    var body = "username=urcoqo00kebdpb4p&
+                password=&
+                text=${this.talkedText}&
+                speaker=show&
+                format=wav&
+                emotion=happiness&
+                emotion_level=2&
+                pitch=100&
+                speed=100&
+                volume=100";
+
+    this.http.post('https://api.voicetext.jp/v1/tts', body)
+      .subscribe(
+        res => {
+          console.log(res);
+          this.result = res;
+        }
+      );
+  }
+
+
 }
